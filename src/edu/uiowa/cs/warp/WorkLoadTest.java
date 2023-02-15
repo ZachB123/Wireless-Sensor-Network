a957b584b85c432c243077cd85e1da90d923dfda
nqlawrence <noah-lawrence@uiowa.edu>
2023-02-15 09:16:01 -0600

    initial commit for workload test

---
+++ src/edu/uiowa/cs/warp/WorkLoadTest.java
@@ -0,0 +1,64 @@
+package edu.uiowa.cs.warp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WorkLoadTest {
+
+	@Test
+	void testAddFlow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFlowPriority() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFlowDeadline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFlowPriorityString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFlowNames() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNodeIndex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNodesInFlow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetHyperPeriod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTotalTxAttemptsInFlow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNumTxAttemptsPerLink() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMaxFlowLength() {
+		fail("Not yet implemented");
+	}
+
+}
