2132969f92410d0e1abb85ba3959445304521896
Zach <zach.buchholz7@gmail.com>
2023-03-28 19:48:00 -0500

    javadoc

--- doc/tag-search-index.js
+++ doc/tag-search-index.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 tagSearchIndex = [];updateSearchResults();
-=======
-tagSearchIndex = [{"l":"Constant Field Values","h":"","u":"constant-values.html"}];updateSearchResults();
->>>>>>> HW3
